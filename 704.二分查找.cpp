96901bc98384e3ff250e831c45120c7666d1ce0b
chaolei.fu <1310866178@qq.com>
2023-01-25 22:58:05 +0800

    tt

--- 704.二分查找.cpp
+++ 704.二分查找.cpp
@@ -49,7 +49,26 @@ class Solution
 public:
     int search(vector<int> &nums, int target)
     {
-        if (nu)
+        int n = nums.size();
+        int mid;
+        int l = 0, r = n - 1;
+        while (l <= r)
+        {
+            mid = l + (r - l + 1) / 2;
+            if (nums[mid] == target)
+            {
+                return mid;
+            }
+            else if (nums[mid] < target)
+            {
+                l = mid + 1;
+            }
+            else
+            {
+                r = mid - 1;
+            }
+        }
+        return -1;
     }
 };
 // @lc code=end
